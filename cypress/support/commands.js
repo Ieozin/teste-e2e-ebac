22d9832a876d1d7398b720176d3c20e386bef071
leonardo <leonardomartinslm9090@gmail.com>
2024-11-28 21:49:03 -0300

    Primeiro comiit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,14 @@
-Cypress.Commands.add('login', (usuario, senha) => {
-    cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
-    cy.get('.woocommerce-form > .button').click()
-});
+class ProdutosPage {
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get('.button-variable-item-' + cor).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+    }
 
+    buscarProdutoLista(nomeProduto) {
+        cy.get('.product-block').contains(nomeProduto).click()
+    }
+}
+
+export default new ProdutosPage()
